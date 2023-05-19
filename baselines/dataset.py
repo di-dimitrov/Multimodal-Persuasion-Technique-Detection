513a799e6451862a0d60a5fe399cd55e37becebc
didimitrov <d.dimitrov.external@sportradar.com>
2023-05-19 14:02:27 +0300

    update datset

--- baselines/dataset.py
+++ baselines/dataset.py
@@ -1,35 +1,44 @@
+import json
 import torch 
 import torchvision
 from torch.utils.data import Dataset
 
-def load_data(data_dir):
-    ...
-    pass
-
 class PropagandaDataset(Dataset):
-    def __init__(self,data_dir,image_processor=None,text_processor,labels):
+    def __init__(self,data_dir,class_list, text_processor = None,image_processor=None):
       super(Dataset, self).__init__()
-      self.data = load_data(data_dir)
+      self.data = self.load_data(data_dir)
       self.image_processor = image_processor
       self.text_processor = text_processor
-      self.labels = labels
+      self.labels = {k: v for v, k in enumerate(class_list)}
         
     def __len__(self):
         return len(self.data)
-
+        
+    def load_data(self,data_dir):
+        with open(data_dir, 'r', encoding='utf-8') as f:
+            return json.load(f)
+        
     def __getitem__(self, idx):
         item = {}
-        item.text = self.text_processor(data[idx]["text"])
-      
-        if "batch_2" in sample_info['id']:
-            id = int(sample_info['id'].split("_batch_2")[0]) + 2000
+        sample = self.data[idx]
+        text = sample["text"]
+        if self.text_processor:
+            text = self.text_processor(text)
+        item['text'] = text
+         
+        if "batch_2" in sample['id']:
+            id = int(sample['id'].split("_batch_2")[0]) + 2000
         else:
-            id = int(sample_info['id'])
-        item.id = torch.tensor(id, dtype=torch.int)
+            id = int(sample['id'])
+        item['id'] = torch.tensor(id, dtype=torch.int)
 
-        label = torch.zeros(22)
-        label[[self.labels[tgt] for tgt in sample_info["labels"]]] = 1
-        item.labels = label
+        lbls = torch.zeros(len(self.labels))
+        lbls[[self.labels[tgt] for tgt in sample["labels"]]] = 1
+        item['labels'] = lbls
         
-        item.image = self.image_processor(self.data[idx]["image"])
+        img = sample["image"]
+        if self.image_processor:
+            img = self.text_processor(img)
+        item['image'] = img
+        
         return item
